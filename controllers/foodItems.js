ae56a3e28b1387d1e2f3985ff03be36752ea7724
Felix <andrewchris99@yahoo.com>
2024-02-27 09:11:14 -0500

    Add API controllers and routes- by region

--- controllers/foodItems.js
+++ controllers/foodItems.js
@@ -1,3 +1,21 @@
+// These contain meal names and pictures //
+
+async function fetchAmerica() {
+  const response = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/filter.php?a=American"
+  );
+  const america = await response.json();
+  console.log(america);
+}
+
+async function fetchBritain() {
+  const response = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/filter.php?a=british"
+  );
+  const britain = await response.json();
+  console.log(britain);
+}
+
 async function fetchCanada() {
   const response = await fetch(
     "https://www.themealdb.com/api/json/v1/1/filter.php?a=Canadian"
@@ -6,6 +24,206 @@ async function fetchCanada() {
   console.log(canada);
 }
 
+async function fetchChina() {
+  const response = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/filter.php?a=chinese"
+  );
+  const china = await response.json();
+  console.log(china);
+}
+
+async function fetchCroatia() {
+  const response = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/filter.php?a=Croatian"
+  );
+  const croatia = await response.json();
+  console.log(croatia);
+}
+
+async function fetchDutch() {
+  const response = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/filter.php?a=Dutch"
+  );
+  const dutch = await response.json();
+  console.log(dutch);
+}
+
+async function fetchEgypt() {
+  const response = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/filter.php?a=Egyptian"
+  );
+  const egypt = await response.json();
+  console.log(egypt);
+}
+
+async function fetchPhillipeans() {
+  const response = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/filter.php?a=Filipino"
+  );
+  const phillipeans = await response.json();
+  console.log(phillipeans);
+}
+
+async function fetchFrance() {
+  const response = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/filter.php?a=French"
+  );
+  const france = await response.json();
+  console.log(france);
+}
+
+async function fetchGreek() {
+  const response = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/filter.php?a=Greek"
+  );
+  const greek = await response.json();
+  console.log(greek);
+}
+
+async function fetchIndia() {
+  const response = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/filter.php?a=Indian"
+  );
+  const india = await response.json();
+  console.log(india);
+}
+
+async function fetchIreland() {
+  const response = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/filter.php?a=Irish"
+  );
+  const ireland = await response.json();
+  console.log(ireland);
+}
+
+async function fetchItaly() {
+  const response = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/filter.php?a=Italian"
+  );
+  const italy = await response.json();
+  console.log(italy);
+}
+
+async function fetchJamaica() {
+  const response = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/filter.php?a=Jamaican"
+  );
+  const jamaica = await response.json();
+  console.log(jamaica);
+}
+
+async function fetchJapan() {
+  const response = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/filter.php?a=Japanese"
+  );
+  const japan = await response.json();
+  console.log(japan);
+}
+
+async function fetchMalaysia() {
+  const response = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/filter.php?a=Malaysian"
+  );
+  const malaysia = await response.json();
+  console.log(malaysia);
+}
+
+async function fetchMexico() {
+  const response = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/filter.php?a=Mexican"
+  );
+  const mexico = await response.json();
+  console.log(mexico);
+}
+
+async function fetchMorocco() {
+  const response = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/filter.php?a=Moroccan"
+  );
+  const morocco = await response.json();
+  console.log(morocco);
+}
+
+async function fetchPoland() {
+  const response = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/filter.php?a=Polish"
+  );
+  const poland = await response.json();
+  console.log(poland);
+}
+
+async function fetchPortugal() {
+  const response = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/filter.php?a=Portuguese"
+  );
+  const portugal = await response.json();
+  console.log(portugal);
+}
+
+async function fetchRussia() {
+  const response = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/filter.php?a=Russian"
+  );
+  const russia = await response.json();
+  console.log(russia);
+}
+
+async function fetchSpain() {
+  const response = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/filter.php?a=Spanish"
+  );
+  const spain = await response.json();
+  console.log(spain);
+}
+
+async function fetchThailand() {
+  const response = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/filter.php?a=Thai"
+  );
+  const thailand = await response.json();
+  console.log(thailand);
+}
+
+async function fetchTunisia() {
+  const response = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/filter.php?a=Tunisian"
+  );
+  const tunisia = await response.json();
+  console.log(tunisia);
+}
+
+async function fetchVietnam() {
+  const response = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/filter.php?a=Vietnamese"
+  );
+  const vietnam = await response.json();
+  console.log(vietnam);
+}
+
 module.exports = {
   fetchCanada,
+  fetchAmerica,
+  fetchBritain,
+  fetchChina,
+  fetchCroatia,
+  fetchDutch,
+  fetchEgypt,
+  fetchPhillipeans,
+  fetchFrance,
+  fetchGreek,
+  fetchIndia,
+  fetchIreland,
+  fetchItaly,
+  fetchJamaica,
+  fetchJapan,
+  fetchMalaysia,
+  fetchMexico,
+  fetchMorocco,
+  fetchPoland,
+  fetchPortugal,
+  fetchRussia,
+  fetchSpain,
+  fetchThailand,
+  fetchTunisia,
+  fetchVietnam,
 };
--- routes/api.js
+++ routes/api.js
@@ -3,10 +3,226 @@ var router = express.Router();
 const foodCtrl = require("../controllers/foodItems");
 const fetch = require("node-fetch");
 
+router.get("/america", async (req, res) => {
+  try {
+    const americaFood = await foodCtrl.fetchAmerica();
+    res.render("region/america", { americaFood });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.get("/britain", async (req, res) => {
+  try {
+    const britainFood = await foodCtrl.fetchBritain();
+    res.render("region/britain", { britainFood });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 router.get("/canada", async (req, res) => {
   try {
     const canadaFood = await foodCtrl.fetchCanada();
-    res.render("canada", { canadaFood });
+    res.render("region/canada", { canadaFood });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.get("/china", async (req, res) => {
+  try {
+    const chinaFood = await foodCtrl.fetchChina();
+    res.render("region/china", { chinaFood });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.get("/croatia", async (req, res) => {
+  try {
+    const croatiaFood = await foodCtrl.fetchCroatia();
+    res.render("region/croatia", { croatiaFood });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.get("/dutch", async (req, res) => {
+  try {
+    const dutchFood = await foodCtrl.fetchDutch();
+    res.render("region/dutch", { dutchFood });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.get("/egypt", async (req, res) => {
+  try {
+    const egyptFood = await foodCtrl.fetchEgypt();
+    res.render("region/egypt", { egyptFood });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.get("/phillipeans", async (req, res) => {
+  try {
+    const phillipeansFood = await foodCtrl.fetchPhillipeans();
+    res.render("region/phillipeans", { phillipeansFood });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.get("/france", async (req, res) => {
+  try {
+    const franceFood = await foodCtrl.fetchFrance();
+    res.render("region/france", { franceFood });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.get("/greek", async (req, res) => {
+  try {
+    const greekFood = await foodCtrl.fetchGreek();
+    res.render("region/greek", { greekFood });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.get("/india", async (req, res) => {
+  try {
+    const indiaFood = await foodCtrl.fetchIndia();
+    res.render("region/india", { indiaFood });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.get("/ireland", async (req, res) => {
+  try {
+    const irelandFood = await foodCtrl.fetchIreland();
+    res.render("region/ireland", { irelandFood });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.get("/italy", async (req, res) => {
+  try {
+    const italyFood = await foodCtrl.fetchItaly();
+    res.render("region/italy", { italyFood });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.get("/jamaica", async (req, res) => {
+  try {
+    const jamaicaFood = await foodCtrl.fetchJamaica();
+    res.render("region/jamaica", { jamaicaFood });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.get("/japan", async (req, res) => {
+  try {
+    const japanFood = await foodCtrl.fetchJapan();
+    res.render("region/japan", { japanFood });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.get("/malaysia", async (req, res) => {
+  try {
+    const malaysiaFood = await foodCtrl.fetchMalaysia();
+    res.render("region/malaysia", { malaysiaFood });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.get("/mexico", async (req, res) => {
+  try {
+    const mexicoFood = await foodCtrl.fetchMexico();
+    res.render("region/mexico", { mexicoFood });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.get("/morocco", async (req, res) => {
+  try {
+    const moroccoFood = await foodCtrl.fetchMorocco();
+    res.render("region/morocco", { moroccoFood });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.get("/poland", async (req, res) => {
+  try {
+    const polandFood = await foodCtrl.fetchPoland();
+    res.render("region/poland", { polandFood });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.get("/portugal", async (req, res) => {
+  try {
+    const portugalFood = await foodCtrl.fetchPortugal();
+    res.render("region/portugal", { portugalFood });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.get("/russia", async (req, res) => {
+  try {
+    const russiaFood = await foodCtrl.fetchRussia();
+    res.render("region/russia", { russiaFood });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.get("/spain", async (req, res) => {
+  try {
+    const spainFood = await foodCtrl.fetchSpain();
+    res.render("region/spain", { spainFood });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.get("/thailand", async (req, res) => {
+  try {
+    const thailandFood = await foodCtrl.fetchThailand();
+    res.render("region/thailand", { thailandFood });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.get("/tunisia", async (req, res) => {
+  try {
+    const tunisiaFood = await foodCtrl.fetchTunisia();
+    res.render("region/tunisia", { tunisiaFood });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.get("/vietnam", async (req, res) => {
+  try {
+    const vietnamFood = await foodCtrl.fetchVietnam();
+    res.render("region/vietnam", { vietnamFood });
   } catch (err) {
     console.log(err);
   }
