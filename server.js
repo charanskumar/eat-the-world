46e0ce1f82e33226f681fc8e9951dd0df432d091
charanskumar <charansenthilk@gmail.com>
2024-02-27 16:05:58 -0500

    Resolve conflicts

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ require('./config/passport');
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+
 const cuisinesRouter = require("./routes/cuisines");
 
 var app = express();
@@ -42,6 +43,7 @@ app.use(function (req, res, next) {
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+
 app.use("/cuisine", cuisinesRouter);
 
 // catch 404 and forward to error handler
