4e3dc36576b21dce870bf6e69eab0e3d00e223e5
dalia <awawdeh.dalia@gmail.com>
2024-02-28 08:28:02 -0600

    update UI

--- server.js
+++ server.js
@@ -4,15 +4,13 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var session = require("express-session");
-var passport = require("passport");
+
 
 require("dotenv").config();
 require("./config/database");
-require("./config/passport");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
-var apiRouter = require("./routes/api");
 
 const cuisinesRouter = require("./routes/cuisines");
 
@@ -27,6 +25,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use('/public', express.static(__dirname + "/public"));
+
 
 app.use(
   session({
@@ -36,8 +36,6 @@ app.use(
   })
 );
 
-app.use(passport.initialize());
-app.use(passport.session());
 
 app.use(function (req, res, next) {
   res.locals.user = req.user;
@@ -46,7 +44,6 @@ app.use(function (req, res, next) {
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-app.use("/food", apiRouter);
 
 app.use("/cuisine", cuisinesRouter);
 
